d11d8912bf5a004e282598b81f5881a1da69b74a
yvis trindade <yvis.darsouls@gmail.com>
2024-09-13 21:35:25 -0300

    variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,23 @@
+/**
+ * estudo de constante de variaveis
+ * @author yvis trindade
+ * 
+ */
+console.clear
+
+//vairaveis
+let nome
+let idade
+let vip
+
+nome = "yvistrindade"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = 22
+console.log(typeof(idade))
+console.log(idade) 
+
+vip = true
+console.log(typeof(vip))
+console.log(vip)
