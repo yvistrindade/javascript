268b457e395a489d616bb2e0be1c64bf29c02211
yvis trindade <yvis.darsouls@gmail.com>
2024-09-19 22:15:57 -0300

    exercicios

---
+++ Exercicios/academia.js
@@ -0,0 +1,29 @@
+/**
+ * ficha de um aluno de academia3
+ * @author yvis trindade
+ */
+
+// variaveis 
+let nome
+let idade
+let peso
+let altura
+let vip
+
+
+// entradada de dados
+console.clear ()
+nome = "yvis trindade"
+idade = 22
+peso = 60
+altura = 1.74
+vip = true
+
+// saida
+console.log("ficha do aluno")
+console.log("______________________")
+console.log(`nome: ${nome}`)
+console.log(`idade: ${idade}`)
+console.log(`peso: ${peso}`)
+console.log(`altura: ${altura}`)
+console.log(`vip: ${vip}`)
