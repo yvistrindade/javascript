fcf5438a2d7fbe1b3a8d79cbfc8d396b4cbc826c
yvis trindade <yvis.darsouls@gmail.com>
2024-09-26 19:50:20 -0300

    atulizaçao de aula

--- fundamentos/sintaxe1.js
+++ fundamentos/sintaxe1.js
@@ -9,11 +9,28 @@ console.clear()
 // Js - sentença
 console.log("hello world")
 
-// concatenação  (unir textos e variaveis)
 let nome = "yvis trindade"
-console.log(`nome: ${nome}`)
-console.log("nome: " + nome) // nao recomendado
+let idade = 22
+// O ponto é usado para casas decimais
+let altura = 1.74
+// typeof() é usado para identificar o tipo de variável
+console.log(typeof(nome))
+console.log(typeof(idade))
+console.log(typeof(altura))
+
+// identificando variáveis inteiras e não inteiras
+console.log(Number.isInteger(idade))
+console.log(Number.isInteger(altura))
+
+// concatenação (unir textos e variáveis)
+console.log(`Nome: ${nome}`)
+console.log("Nome: " + nome) // não recomendado
 
 // Arrendodamneto (casas decimais)
 let media = 5.789
 console.log(`media: ${media.tofixed(1)}`)
+
+// particularidade do JS 
+console.log(10/0)
+console.log(0.5 + 0.5)
+console.log(0.1 + 0.2)
