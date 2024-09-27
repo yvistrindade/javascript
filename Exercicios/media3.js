54d14e2cb86d0c6f1291b4b9c2bf1395b395cd6c
yvis trindade <yvis.darsouls@gmail.com>
2024-09-27 20:03:08 -0300

    atulizaçao de aula

---
+++ Exercicios/media3.js
@@ -0,0 +1,24 @@
+/**
+ * calculo da media aritimetica de 3 valores
+ * @author yvis trindade
+ */
+
+//importar a biblioteca read-linesync
+const input = require(`readline-sync`)
+
+//variaveis
+let num1, num2 , num3, MediaCapabilities
+
+console.clear()
+console.log("calculo da media aritimetica de 3 numeros")
+
+//entrada
+num1 = Number(input.question("digite o primeiro numero:"))
+num2 = Number(input.question("digite o segundo numero:"))
+num3 = Number(input.question("digite o terceiro numero:"))
+
+//processando
+media = (num1 + num2 + num3) / 3
+
+//saida
+console.log(`media: ${media.toFixed(1)}`)
